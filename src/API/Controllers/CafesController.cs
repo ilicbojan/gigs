8a039af12af8645cf2661f3f3d0d15c83489ca0f
ilicbojan <bojan.ilic2404@gmail.com>
2021-04-13 19:04:47 +0200

    Add UpdateCafe Feature

--- src/API/Controllers/CafesController.cs
+++ src/API/Controllers/CafesController.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Cafes.Commands.CreateCafe;
+using Application.Cafes.Commands.UpdateCafe;
 using Application.Cafes.Queries.GetCafeById;
 using Application.Cafes.Queries.GetCafesList;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -23,6 +25,14 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult<int>> Create(CreateCafeCommand command)
         {
+            return await Mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Update(int id, UpdateCafeCommand command)
+        {
+            command.Id = id;
+
             return await Mediator.Send(command);
         }
     }
---
+++ src/Application/Cafes/Commands/UpdateCafe/UpdateCafeCommand.cs
@@ -0,0 +1,49 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Cafes.Commands.UpdateCafe
+{
+    public class UpdateCafeCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class UpdateCafeCommandHandler : IRequestHandler<UpdateCafeCommand>
+    {
+        private readonly IAppDbContext _context;
+
+        public UpdateCafeCommandHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateCafeCommand request, CancellationToken cancellationToken)
+        {
+            var cafe = await _context.Cafes.FindAsync(request.Id);
+
+            if (cafe == null)
+            {
+                throw new NotFoundException(nameof(Cafe), request.Id);
+            }
+
+            cafe.Name = request.Name;
+            cafe.City = request.City;
+            cafe.Address = request.Address;
+            cafe.Email = request.Email;
+            cafe.Phone = request.Phone;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
